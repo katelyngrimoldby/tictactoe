4a0a5f9e363465b95f731f2bb196e43f69be820d
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-05-08 17:45:29 -0600

    Add player factory & mark placement

--- app.js
+++ app.js
@@ -2,10 +2,50 @@ const gameBoard = (() => {
     const initBoard = (arr) => {
         for (i = 0; i < 9; i++) {
             id = (i+1).toString();
-            arr[i] = document.getElementById(id)
+            arr[i] = document.getElementById(id);
         }
-        return arr
+        return arr;
     }
-    let boardArr = []
-    return {initBoard, boardArr};
+
+    const renderMark = (id, mark, color) => {
+        boardArr[id-1].style.color = color;
+        boardArr[id-1].textContent = mark;
+    }
+
+    let boardArr = [];
+    return {initBoard, renderMark, boardArr};
 })();
+
+const gameManager = (() => {
+    let turn = true //true for X, false for O
+    const makeMove = (e) => {
+        let id = e.target.id;
+        if (turn) {
+            gameBoard.renderMark(id, x.getMark(), x.getColor());
+        }else {
+            gameBoard.renderMark(id, o.getMark(), o.getColor())
+        }
+        turn = !turn
+    }
+    return {makeMove}
+})();
+
+const Player = (marker, color) => {
+    const mark = marker;
+    const markColor = color;
+
+    const getMark = () => mark;
+
+    const getColor = () => color;
+
+    return {getMark, getColor};
+}
+
+const x = Player('X', 'red')
+const o = Player('O', 'blue')
+
+window.onload = gameBoard.initBoard(gameBoard.boardArr)
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', gameManager.makeMove)
